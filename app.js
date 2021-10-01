86cba9fc3431d59d0780d3b4f2dbe7e0c36cb9cf
SagarVartak <34567173+SagarVartak@users.noreply.github.com>
2021-10-02 00:16:22 +0530

    jnjnjln

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var axios = require("axios");
+const { response } = require("express");
+const app = express();
+app.get("/", async (req, res) => {
+    // res.send("Welcome");
+    var newdata = await axios.get("https://api.imagekit.io/v1/files",{
+        auth:{
+            username:"private_FEQHGrJPnn7Nl/Ymv+UByhUB4jE=",
+            password:"Sagar@992370"
+        },
+        headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+                 },
+                 crossdomain: true
+    }).then(response => {
+        // console.log("respone",response);
+        return response.data;
+    }).catch(err => {
+        console.log(err);
+    })
+    console.log(newdata);
+    res.json(newdata);
+  });
+  
+  app.listen(process.env.PORT || 3000, function () {
+    console.log(
+      "Express server listening on port %d in %s mode",
+      this.address().port,
+      app.settings.env
+    );
+  });
